5a5ced7919afdc1c6f1ba4899ec1cd8e62fb08eb
RahuLR <65016146+RahulRgowdru@users.noreply.github.com>
2021-09-03 21:07:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ document.getElementById('output').style.visibility ='hidden'
 document.getElementById('inInput').addEventListener('input', function(e){
 document.getElementById('output').style.visibility ='visible'
 let pounds = e.target.value;
-document.getElementById('gramsOutput').innerHTML = pounds/0.0022046;
-document.getElementById('kilogramsOutput').innerHTML = pounds/2.2046;
-document.getElementById('ouncessOutput').innerHTML = pounds*16;
+document.getElementById('gramsOutput').innerHTML = (pounds/0.0022046).toFixed(2);
+document.getElementById('kilogramsOutput').innerHTML = (pounds/2.2046).toFixed(2);
+document.getElementById('ouncessOutput').innerHTML = (pounds*16).toFixed(2);
 
 });
